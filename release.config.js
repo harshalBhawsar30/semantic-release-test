401726f09c4bf2cecf168e62fd1b666d90ed7498
satyapatidar <spatidar@bidgely.com>
2025-07-18 17:28:26 +0530

    feat: Testing version bump in package.json

--- release.config.js
+++ release.config.js
@@ -12,20 +12,21 @@ module.exports = {
     ],
     [
       './scripts/external-changelog-generator.js',
-      {}, // it handles filename dynamically now
+      {},
     ],
+    '@semantic-release/npm', // ✅ This bumps version in package.json
     [
       '@semantic-release/git',
       {
         assets: [
           'CHANGELOG.md',
           'package.json',
-          'external-*.md', // ✅ commit versioned external changelog file
+          'external-*.md',
         ],
         message:
           'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
     ],
-    '@semantic-release/github', // ✅ no external changelog here
+    '@semantic-release/github',
   ],
 };
